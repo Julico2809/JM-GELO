e159422a7209f040a4bb66a68e09f79e0870200a
Julico2809 <adegajm01@gmail.com>
2025-07-21 00:59:15 -0300

    Add files via upload

--- login.js
+++ login.js
@@ -1,1 +1,11 @@
-// Script funcional: login.js
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+  const user = document.getElementById("usuario").value;
+  const pass = document.getElementById("senha").value;
+
+  if (user === "JMGELO" && pass === "JMGELO") {
+    window.location.href = "painel.html";
+  } else {
+    alert("Usuário ou senha inválidos");
+  }
+});
